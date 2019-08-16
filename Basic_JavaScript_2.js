25ba642859a33a247ec54b4fa265297e70bb9260
NQuinzon <51680780+NQuinzon@users.noreply.github.com>
2019-08-16 15:37:40 -0700

    Add files via upload

--- Basic_JavaScript_2.js
+++ Basic_JavaScript_2.js
@@ -3,3 +3,8 @@ function My_First_Function () {
     var result = String.fontcolor("blue");
     document.getElementById("Blue_Text").innerHTML = result;
 }
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("concatenante").innerHTML = sentence;
+}
